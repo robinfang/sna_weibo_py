eb09f77de61b97e6c0d47ed0122de3b674fd5382
robinfang <thefmajia@gmail.com>
2013-06-10 19:36:22 +0800

    正确地将"n分钟前"、"今天 HH:mm"这两种格式的时间解析存入repost_time 下一步需要对"MM月DD日"这种格式进行处理

--- weibo_repo_crawler.py
+++ weibo_repo_crawler.py
@@ -9,30 +9,30 @@ import re
 import lxml.html.soupparser as soupparser
 import datetime
 class WeiboPost(object):
-	"""一条新浪微博。
-	Attributes:
-		mid: 字符串，微博的mid，形如zAjoQmY0n
-		uid: 字符串发布者id
-		post_time: 发布时间
-		content: 字符串内容
-		repost_list: WeiboReply回复列表
-	"""
-	def __init__(self, mid=None, uid=None, post_time=None, content=None):
-		self.mid = mid
-		self.uid = uid
-		self.post_time = post_time
-		self.content = content
+    """一条新浪微博。
+    Attributes:
+        mid: 字符串，微博的mid，形如zAjoQmY0n
+        uid: 字符串发布者id
+        post_time: 发布时间
+        content: 字符串内容
+        repost_list: WeiboReply回复列表
+    """
+    def __init__(self, mid=None, uid=None, post_time=None, content=None):
+        self.mid = mid
+        self.uid = uid
+        self.post_time = post_time
+        self.content = content
 class WeiboReply(object):
-	"""一条微博回复。
-	Attributes:
-		user_url: 字符串用户链接
-		content: 用户转发内容
-		repost_time: 用户转发时间
-	"""
-	def __init__(self, user_url=None, content=None, repost_time=None):
-		self.user_url = user_url
-		self.content = content
-		self.repost_time = repost_time
+    """一条微博回复。
+    Attributes:
+        user_url: 字符串用户链接
+        content: 用户转发内容
+        repost_time: 用户转发时间
+    """
+    def __init__(self, user_url=None, content=None, repost_time=None):
+        self.user_url = user_url
+        self.content = content
+        self.repost_time = repost_time
 mid = "zAjoQmY0n"
 gsid = "4uwgb764149TppfO8K622703C8g"
 url = "http://weibo.cn/repost/"
@@ -42,13 +42,6 @@ data = urllib2.urlopen(request).read()
 f = open('repo.xml','w')
 f.write(data)
 f.close()
-#parser = etree.XMLParser(remove_blank_text=True, resolve_entities=False)
-#tree = etree.parse(StringIO(data),parser)
-#divs = tree.xpath("//*[@class='c']")
-#for j in divs:
-#	for i in j.xpath('./*/text()'):
-#		print i
-#demo = divs[2].xpath('node()')
 dom = soupparser.fromstring(data)
 divs = dom.xpath("//*[@class='c']")
 weibo_post = WeiboPost()
@@ -68,9 +61,19 @@ for i in range(0,len(divs)):
     print time_string
     time_string_split = time_string.split()
     if len(time_string_split)==1:
-        minutes = re.compile(ur'(.*)\u5206.*').match(time_string_split[0]).group(1)
-        print minutes
+        minutes = re.compile(ur'(.*)\u5206.*').match(time_string_split[0]).group(1)    #取得转发距现在过去了几分钟
+        #print minutes
         weibo_reply.repost_time = datetime.datetime.today()\
                 +datetime.timedelta(minutes = 0-int(minutes))
-        print weibo_reply.repost_time
-        weibo_post.repost_list.append(weibo_reply)
+       
+    elif u'\u4eca\u5929' in time_string_split:
+        time = time_string_split[1]    #取得转发时间HH:mm
+        hour,minutes = time.split(":")
+        today = datetime.date.today()
+        weibo_reply.repost_time = datetime.datetime(today.year,\
+                today.month,\
+                today.day,\
+                int(hour),int(minutes))
+                
+    print "repost_time : %s"%weibo_reply.repost_time
+    weibo_post.repost_list.append(weibo_reply)
