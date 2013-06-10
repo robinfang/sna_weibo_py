3714a58ec53d495c1cfb8d89d7d581bfb123c73c
robinfang <thefmajia@gmail.com>
2013-06-10 22:37:57 +0800

    finished extraction in 4 cases for one page. doing all pages for 1 weibo needed

--- weibo_repo_crawler.py
+++ weibo_repo_crawler.py
@@ -33,10 +33,10 @@ class WeiboReply(object):
         self.user_url = user_url
         self.content = content
         self.repost_time = repost_time
-mid = "zAjoQmY0n"
+mid = "yci8hkTUf"
 gsid = "4uwgb764149TppfO8K622703C8g"
 url = "http://weibo.cn/repost/"
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:21.0) Gecko/20100101 Firefox/21.0"}
+headers = {"user-agent":"mozilla/5.0 (windows nt 6.1; rv:21.0) gecko/20100101 firefox/21.0"}
 request = urllib2.Request("%s%s?&gsid=%s"%(url,mid,gsid),headers=headers)
 data = urllib2.urlopen(request).read()
 f = open('repo.xml','w')
@@ -60,13 +60,16 @@ for i in range(0,len(divs)):
     time_string = re.compile(ur'(.*)\u6765.*').match(nodes[-1].text).group(1).strip()
     print time_string
     time_string_split = time_string.split()
+    match_ymd = re.compile(ur'(\d{4})-(\d{2})-(\d{2})').match(time_string_split[0])
     if len(time_string_split)==1:
+        print "case1"
         minutes = re.compile(ur'(.*)\u5206.*').match(time_string_split[0]).group(1)    #取得转发距现在过去了几分钟
         #print minutes
         weibo_reply.repost_time = datetime.datetime.today()\
                 +datetime.timedelta(minutes = 0-int(minutes))
-       
+
     elif u'\u4eca\u5929' in time_string_split:
+        print "case2"
         time = time_string_split[1]    #取得转发时间HH:mm
         hour,minutes = time.split(":")
         today = datetime.date.today()
@@ -74,6 +77,31 @@ for i in range(0,len(divs)):
                 today.month,\
                 today.day,\
                 int(hour),int(minutes))
-                
+    elif u'\u6708' in time_string_split[0] and u'\u65e5' in time_string_split[0]:
+        print "case3"
+        date_string = time_string_split[0]
+        match = re.compile(ur'(\d{2}).*(\d{2}).*').match(date_string)
+        today = datetime.date.today()
+        month = match.group(1)
+        day = match.group(2)
+        time = time_string_split[1]
+        hour,minutes = time.split(":")
+        #print "nomth,day : %s, %s "% (month,day)
+        weibo_reply.repost_time = datetime.datetime(today.year,\
+                int(month),\
+                int(day),\
+                int(hour),int(minutes))
+    elif match_ymd!=None:
+        print "case4"
+        year = match_ymd.group(1)
+        month = match_ymd.group(2)
+        day = match_ymd.group(3)
+        #print "year-month-day:%s,%s,%s"%(year,month,day)
+        time = time_string_split[1]
+        hour,minutes,_ = time.split(":")
+        weibo_reply.repost_time = datetime.datetime(int(year),\
+                int(month),\
+                int(day),\
+                int(hour),int(minutes))
     print "repost_time : %s"%weibo_reply.repost_time
     weibo_post.repost_list.append(weibo_reply)
