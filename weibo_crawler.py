ca5e8bf60542df5e8e2ab58f49f30ac20183f39d
robinfang <thefmajia@gmail.com>
2013-08-09 10:51:25 +0800

    修改getReplies(url)，分析转发所附评论

--- weibo_crawler.py
+++ weibo_crawler.py
@@ -1,6 +1,14 @@
 #coding=utf-8
+import urllib2
+from lxml import etree
+from StringIO import StringIO
+import re
+import lxml.html.soupparser as soupparser
+import datetime
+
 class User(object):
     """微博用户
+    
     Attributes:
         url: 用户url
         uid: 用户id
@@ -23,6 +31,7 @@ class User(object):
     
 class WeiboPost(object):
     """一条新浪微博。
+    
     Attributes:
         mid: 字符串，微博的mid，形如zAjoQmY0n
         user: 字符串发布者
@@ -30,27 +39,58 @@ class WeiboPost(object):
         content: 字符串内容
         repost_list: WeiboReply回复列表
     """
-    def __init__(self, mid=None, user=None, post_time=None, content=None):
+    def __init__(self, mid = None, user = None, post_time = None, content = None):
         self.mid = mid
         self.user = user
         self.post_time = post_time
-		self.content = content
+        self.content = content
 class WeiboRepost:
     """一条微博回复。
+    
     Attributes:
         content: 内容
         time: 时间
         user: 发布者
         from_user: 转发来源
     """
-    def __init__(self, content=None, time=None, user=None, from_user=None):
+    def __init__(self, content = None, time = None, user = None, from_user = None):
         self.content = content
         self.time = time
         self.user = user
         self.from_user = from_user
 
-def get_replies(url):
+        
+class WeiboParser:
+    """页面分析器
+    
+    Attributes： 
+        headers: 请求头
+        url: 链接
+        state: 状态
+        pages_number: 一共有多少页
+        repost_list: 转发列表
+    """
+    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:21.0) Gecko/20100101 Firefox/21.0"}
+    def __init__(self, url):
+        self.state = "init"
+        self.url = url
+    def parseOnePage(self):
+        """解析一个页面
+        """
+        list = []
+        request = urllib2.Request(url,headers = headers)
+        data = urllib2.urlopen(request).read()
+        dom = soupparser.fromstring(data)
+     
+        
+def getRepostList(url):
+    """
+    """
+        
+        
+def getReplies(url):
     """以list形式通过url取得一个页面内的回复。
+    
     Args:
         url: 页面的链接字符串，包含page参数、gsid、mid等，形如：http://weibo.cn/repost/yci8hkTUf?&gsid=4uwgb764149TppfO8K622703C8g&page=2
     Returns: 
@@ -58,30 +98,37 @@ def get_replies(url):
     """
     list = []
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:21.0) Gecko/20100101 Firefox/21.0"}
-    request = urllib2.Request(url,headers=headers)
+    request = urllib2.Request(url,headers = headers)
     data = urllib2.urlopen(request).read()
     dom = soupparser.fromstring(data)
     divs = dom.xpath("//*[@class='c']")
+    #global gdivs
+    #gdivs = divs
     for i in range(0,len(divs)):
         nodes  =  divs[i].xpath('node()')
         if len(nodes)==0:
             continue
         elif nodes[-1].tag!='span':
             continue
-        weibo_reply = WeiboReply()
+        weibo_repost = WeiboRepost()
+        for j in range(0,len(nodes)):
+            if isinstance(nodes[j],unicode):
+                content = nodes[j]
+                print type(content)
+                print content
         full_url_string = "weibo.cn%s" % nodes[0].get('href')
-        weibo_reply.user_url = full_url_string
-        weibo_reply.user_url = re.compile(r'(\S*)?\?').match(full_url_string).group(1)
-        print weibo_reply.user_url
+        weibo_repost.user_url = full_url_string
+        weibo_repost.user_url = re.compile(r'(\S*)?\?').match(full_url_string).group(1)
+        print "user url:" + weibo_repost.user_url
         time_string = re.compile(ur'(.*)\u6765.*').match(nodes[-1].text).group(1).strip()
-        print time_string
+        print "time string:" + time_string
         time_string_split = time_string.split()
         match_ymd = re.compile(ur'(\d{4})-(\d{2})-(\d{2})').match(time_string_split[0])
         if len(time_string_split)==1:
             print "case1"
             minutes = re.compile(ur'(.*)\u5206.*').match(time_string_split[0]).group(1)    #取得转发距现在过去了几分钟
             #print minutes
-            weibo_reply.repost_time = datetime.datetime.today()\
+            weibo_repost.repost_time = datetime.datetime.today()\
                     +datetime.timedelta(minutes = 0-int(minutes))
 
         elif u'\u4eca\u5929' in time_string_split:
@@ -89,7 +136,7 @@ def get_replies(url):
             time = time_string_split[1]    #取得转发时间HH:mm
             hour,minutes = time.split(":")
             today = datetime.date.today()
-            weibo_reply.repost_time = datetime.datetime(today.year,\
+            weibo_repost.repost_time = datetime.datetime(today.year,\
                     today.month,\
                     today.day,\
                     int(hour),int(minutes))
@@ -103,7 +150,7 @@ def get_replies(url):
             time = time_string_split[1]
             hour,minutes = time.split(":")
             #print "nomth,day : %s, %s "% (month,day)
-            weibo_reply.repost_time = datetime.datetime(today.year,\
+            weibo_repost.repost_time = datetime.datetime(today.year,\
                     int(month),\
                     int(day),\
                     int(hour),int(minutes))
@@ -115,13 +162,14 @@ def get_replies(url):
             #print "year-month-day:%s,%s,%s"%(year,month,day)
             time = time_string_split[1]
             hour,minutes,_ = time.split(":")
-            weibo_reply.repost_time = datetime.datetime(int(year),\
+            weibo_repost.repost_time = datetime.datetime(int(year),\
                     int(month),\
                     int(day),\
                     int(hour),int(minutes))
-        print "repost_time : %s"%weibo_reply.repost_time
-        list.append(weibo_reply)
+        print "repost_time : %s"%weibo_repost.repost_time
+        list.append(weibo_repost)
     return list
     
-
-
+if __name__ == "__main__":
+    getReplies("http://weibo.cn/repost/yci8hkTUf?&gsid=4uwgb764149TppfO8K622703C8g&page=1&st=df7e")
+ 
